e45d25886d24d05464af5c83b76698d2b12864c9
Tom Pearson <tom_pearson@yahoo.com>
2021-01-10 16:22:56 -0800

    GUI working with buttons. Now start porting the rest of the pocketknife modules

--- PocketKnifeBuildOutx2/app/src/main/java/com/example/pocketknifebuildoutx2/MainActivity.kt
+++ PocketKnifeBuildOutx2/app/src/main/java/com/example/pocketknifebuildoutx2/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.pocketknifebuildoutx2
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.Flags
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+import com.microsoft.appcenter.distribute.Distribute
+import com.microsoft.appcenter.utils.async.AppCenterConsumer
+
 import isItRooted
+val properties: Map<String, String> = HashMap()
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +29,7 @@ class MainActivity : AppCompatActivity() {
         lateinit var crash_button: Button
 
         val builder = StringBuilder()
-        val properties: Map<String, String> = HashMap()
+
         // Setup buttons
         button = findViewById(R.id.button_Map_id)
         button.setText(R.string.title_map_button)
@@ -30,8 +40,81 @@ class MainActivity : AppCompatActivity() {
         textMessage = findViewById(R.id.message)
         textMessage.setTextColor(Color.BLACK)
 
+        // Setup buttons
+        button = findViewById(R.id.button_Map_id)
+        button.setText(R.string.title_map_button)
+
+        crash_button = findViewById(R.id.button_crash_id)
+        crash_button.setText(R.string.button_crash_text)
+
+        textMessage = findViewById(R.id.message)
+        textMessage.setTextColor(Color.BLACK)
+
+        AppCenter.start(
+            application, "e93f7bb5-d8b0-467f-8ea0-d20db09a2cb8",
+            Analytics::class.java, Crashes::class.java, Distribute::class.java
+        )
+        val future = Crashes.hasCrashedInLastSession()
+        future.thenAccept(AppCenterConsumer {
+            if (it) {
+                Analytics.trackEvent(getString(R.string.crash_detected), properties, Flags.CRITICAL)
+                Toast.makeText(this, getText(R.string.crash_oops), Toast.LENGTH_LONG).show()
+            }
+        })
 
 
         textMessage.setText(isItRooted(this, builder))
+
+        crash_button.setOnClickListener {
+            Analytics.trackEvent(
+                getString(R.string.button_crashing_clicked),
+                properties,
+                Flags.CRITICAL
+            )
+            Crashes.generateTestCrash()
+        }
+//        Map feature
+
+//        button.setOnClickListener {
+//            Analytics.trackEvent(getString(R.string.button_map_clicked), properties, Flags.NORMAL);
+//            val intent: Intent = Intent(this, MapsActivity::class.java).apply {}
+//            startActivity(intent)
+//        }
+    }
+    override fun onStop() {
+        super.onStop()
+        Analytics.trackEvent(getString(R.string.onStop), properties, Flags.NORMAL);
+        Toast.makeText(this, getString(R.string.onStop), Toast.LENGTH_LONG)
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Analytics.trackEvent(getString(R.string.onRestart), properties, Flags.NORMAL);
+        Toast.makeText(this, getString(R.string.onRestart), Toast.LENGTH_LONG)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Analytics.trackEvent(getString(R.string.onResume), properties, Flags.NORMAL);
+        Toast.makeText(this, getString(R.string.onResume), Toast.LENGTH_LONG)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Analytics.trackEvent(getString(R.string.onStart), properties, Flags.NORMAL);
+        Toast.makeText(this, getString(R.string.onStart), Toast.LENGTH_LONG).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Analytics.trackEvent(getString(R.string.onPause), properties, Flags.NORMAL);
+        Toast.makeText(this, getString(R.string.onPause), Toast.LENGTH_LONG)
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Analytics.trackEvent(getString(R.string.onDestroy), properties, Flags.NORMAL);
+        Toast.makeText(this, getString(R.string.onDestroy), Toast.LENGTH_LONG)
     }
 }
